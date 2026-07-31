8c86db5e29ffa647d6d1a0ca7949496188622370
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-07-31 23:40:04 +0300

    Fix SECRET_KEY duplication and INSTALLED_APPS typo

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from decouple import config
 
-SECRET_KEY = config('k8d-wg=o%gpf6umczm-%%i+#t0vi*d4(1cw8%bo4js#7to^nbp')
+SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wyo-$92d4d4336wxdd#jw%m8-qty&pi-gtb3-9((twj+mpln1o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -42,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs',
-    'rest_framework'
+    'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
     'corsheaders'
