cdd1ec4e919919b0879cc66bec4cb00fbbc3ea34
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-07-31 23:19:07 +0300

    company model is added

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Company(models.Model):
+    name=models.CharField(max_length=250)
+    description=models.TextField()
+    website=models.URLField()
+    recruiter=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
