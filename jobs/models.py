29f58d19b4774c5ecd00995f440c62dff3b29ee8
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-07-31 23:20:23 +0300

    job model is added

--- jobs/models.py
+++ jobs/models.py
@@ -10,5 +10,21 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
-
-
+class Job(models.Model):
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
+    title=models.CharField(max_length=250)
+    description=models.TextField()
+    location=models.CharField(max_length=250)
+    salary_min=models.DecimalField(max_digits=10, decimal_places=2)
+    salary_max=models.DecimalField(max_digits=10, decimal_places=2)
+    JOB_TYPE_CHOICES = [
+        ('full_time', 'Full-time'),
+        ('part_time', 'Part-time'),
+        ('remote', 'Remote'),
+    ]
+    
+    job_type = models.CharField(max_length=20, choices=JOB_TYPE_CHOICES, default='full_time')
+    is_active=models.BooleanField()
+    deadline=models.DateField()
+    def __str__(self):
+        return self.title
