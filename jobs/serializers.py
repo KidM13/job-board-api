1824449dda2849e0b06e3ef454e55f89b7fe3e24
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-08-01 10:22:25 +0300

    job serializer is added

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -5,3 +5,8 @@ class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model=Company
         fields=['id','name','description','website','recruiter']
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Job
+        fields=['id','title','company','description','location','salary_min','salary_max','job_type','is_active','deadline']
+
