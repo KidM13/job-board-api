b8447cbc6d4796cd18dc6158f5f2cd46ab94c3be
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-08-25 22:56:39 +0300

    setting up throttling

--- core/settings.py
+++ core/settings.py
@@ -83,6 +83,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':3,
+    'DEFAULT_THROTTLE_CLASS':[
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
 }
 
 
