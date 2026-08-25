5b8942705af9e16b7ea00f1dc04d8eb03fe78398
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-08-25 23:05:34 +0300

    finishing the throttling

--- core/settings.py
+++ core/settings.py
@@ -87,6 +87,10 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
     ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '10/minute',
+    }
 }
 
 
