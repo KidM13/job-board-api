bc1da882c6c83dcc0706264485b531df434c5e79
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-08-26 22:46:45 +0300

    setting up a test

--- jobs/tests.py
+++ jobs/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Company, Job, Application
