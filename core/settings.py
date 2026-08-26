f2082dcfa6dd03c79aa61f7e7336b8e95d367cbb
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-08-26 22:38:36 +0300

    a keyname fix in the throttle

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':3,
-    'DEFAULT_THROTTLE_CLASS':[
+    'DEFAULT_THROTTLE_CLASSES':[
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
     ],
