68031ea41efd19fac22dc6cb6f4a43991c607750
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-08-26 23:05:03 +0300

    handling the test function

--- jobs/tests.py
+++ jobs/tests.py
@@ -6,3 +6,14 @@ from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Company, Job, Application
+
+class JobPermissionAPITest(TestCase):
+    def non_owner_of_company_cannot_create_job(self):
+        userA = User.objects.create_user(username='test', password='test123')
+        userB = User.objects.create_user(username='kebede', password='kebede123')
+        companyA=Company.objects.create(
+            name='userA company',
+            description='there is nothing to say about ',
+            
+
+        )
