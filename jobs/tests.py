f5a632d64dcd6efd870773a4cdd9baf5e1977a53
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-08-28 22:15:10 +0300

    a company A information is added

--- jobs/tests.py
+++ jobs/tests.py
@@ -8,12 +8,13 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Company, Job, Application
 
 class JobPermissionAPITest(TestCase):
-    def non_owner_of_company_cannot_create_job(self):
+    def test_non_owner_cannot_create_job(self):
         userA = User.objects.create_user(username='test', password='test123')
         userB = User.objects.create_user(username='kebede', password='kebede123')
         companyA=Company.objects.create(
             name='userA company',
             description='there is nothing to say about ',
-            
-
+            website='http://companyAwebsite.com',
+            recruiter=userA,
         )
+        
