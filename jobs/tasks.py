5db94b5549d052f7c292ee536bad74dc757ede59
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-09-05 15:28:21 +0300

    using a loop approach

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -10,4 +10,13 @@ def notify_recruiter(application_id):
 def deactivate_expired_jobs():
     from django.utils import timezone
     from .models import Job
+    jobs=Job.objects.filter(is_active=True,deadline__lt=timezone.now.date())
+    for job in jobs:
+        job.is_active=False
+        job.save()
+
+        
+    
+        
+
    
