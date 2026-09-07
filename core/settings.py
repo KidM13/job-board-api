14382cda7c2a6ae324924f9a6bf76375151bd804
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-09-07 22:05:14 +0300

    finishing the whitenoise

--- core/settings.py
+++ core/settings.py
@@ -145,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
