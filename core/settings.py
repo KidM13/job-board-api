3147c06db9407576b0fcb22236a094c40a5cd50c
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-09-08 22:29:15 +0300

    fixing the localhost fallback to the redis

--- core/settings.py
+++ core/settings.py
@@ -152,7 +152,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
